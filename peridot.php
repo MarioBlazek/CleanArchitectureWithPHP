4023926002e140d9f2895bb382bcd13cff808397
Mario Blazek <mario.b@netgen.hr>
2016-01-31 15:05:50 +0100

    Installed and configured Prophecy

--- peridot.php
+++ peridot.php
@@ -2,10 +2,13 @@
 
 use Evenement\EventEmitterInterface;
 use Peridot\Plugin\Watcher\WatcherPlugin;
+use Peridot\Plugin\Prophecy\ProphecyPlugin;
 
 return function(EventEmitterInterface $emitter) {
     $watcher = new WatcherPlugin($emitter);
     $watcher->track(__DIR__ . '/src');
+
+    new ProphecyPlugin($emitter);
 };
 
 // to make peridot watch specs dir
