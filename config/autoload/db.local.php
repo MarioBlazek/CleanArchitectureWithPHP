379570bdc0c2ee515cc54020981370760610b554
Mario Blazek <mario.b@netgen.hr>
2016-02-09 07:45:44 +0100

    DoctrineORMModule installed and configured

---
+++ config/autoload/db.local.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'doctrine' => [
+        'connection' => [
+            'orm_default' => [
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOSqlite\Driver',
+                'params' => [
+                    'path' => __DIR__ . '/../../data/database.db'
+                ]
+            ]
+        ],
+    ],
+];
